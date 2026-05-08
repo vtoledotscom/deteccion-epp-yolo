00f67e1264f57a134b1853d51cc516cb871ce88b
valentina <vtoledo@tscom.cl>
2026-05-08 13:19:16 -0400

    fix lenguage in site

---
+++ lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Tu contraseña fue restablecida correctamente.',
+    'sent' => 'Te enviamos el enlace de recuperación de contraseña.',
+    'throttled' => 'Espera antes de intentarlo nuevamente.',
+    'token' => 'El token de recuperación de contraseña no es válido.',
+    'user' => 'No encontramos una cuenta con ese correo electrónico.',
+];
